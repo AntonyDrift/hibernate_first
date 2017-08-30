ddb4b3f5c105e38465cca6464a15dbd4d55b9674
yslabko <yslabko@exadel.com>
2017-08-30 14:10:34 +0300

    Add module lecture_2

---
+++ lecture_2_operations/src/main/java/by/it/entity/Person.java
@@ -0,0 +1,31 @@
+package by.it.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Person implements Serializable {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column
+    private Integer age;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+}
+
